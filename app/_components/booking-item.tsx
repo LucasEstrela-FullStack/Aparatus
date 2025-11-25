73a686e229395fde2882cba5d5c50fa32f3f5b14
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-11-25 01:16:39 -0400

    feat: prompt alert dialog to user before cancelling booking

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -4,7 +4,13 @@ import { Badge } from "./ui/badge";
 import { Card } from "./ui/card";
 import { Avatar } from "./ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { useState } from "react";
 import { PhoneItem } from "./phone-item";
 import Image from "next/image";
@@ -14,6 +20,7 @@ import { cancelBooking } from "../_actions/cancel-booking";
 import { toast } from "sonner";
 import { X } from "lucide-react";
 import { Separator } from "./ui/separator";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
 
 interface BookingItemProps {
   booking: {
@@ -198,13 +205,33 @@ const BookingItem = ({ booking }: BookingItemProps) => {
             Voltar
           </Button>
           {isConfirmed && (
-            <Button
-              variant="destructive"
-              className="flex-1 rounded-full"
-              onClick={handleCancelBooking}
-            >
-              Cancelar Reserva
-            </Button>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant="destructive" className="flex-1 rounded-full">
+                  Cancelar Reserva
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Cancelar reserva
+                  </AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Tem certeza que deseja cancelar esta reserva? Esta ação não
+                    pode ser desfeita.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Voltar</AlertDialogCancel>
+                  <AlertDialogAction
+                    onClick={handleCancelBooking}
+                    className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+                  >
+                    Confirmar
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           )}
         </div>
       </SheetContent>
