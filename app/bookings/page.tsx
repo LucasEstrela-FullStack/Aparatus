a7a003b8b68e33d1aff0baba0a2d31a85a7cd069
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-11-29 14:53:13 -0400

    feat: ensure footer is rendered at the bottom on bookings page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -4,7 +4,7 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import Header from "../_components/header";
 import Footer from "../_components/footer";
-import { PageContainer, PageSection, PageSectionTitle } from "../_components/ui/page";
+import { PageContainer, PageSection,PageSectionTitle } from "../_components/ui/page";
 import BookingItem from "../_components/booking-item";
 
 const BookingsPage = async () => {
